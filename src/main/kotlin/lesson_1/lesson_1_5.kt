a9c6de2c228902880e84c32bf05a4302eb0588ea
Vibriya <ritaboyko906151@gmail.com>
2023-03-23 19:22:47 +0200

    KS-1-5 added time formatting (ss to mm:ss)

--- src/main/kotlin/lesson_1/lesson_1_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_5.kt
@@ -1,2 +1,14 @@
 package lesson_1
 
+fun main() {
+    val seconds: Int = 6480
+    println(dateFormatter(seconds))
+}
+
+fun dateFormatter(seconds: Int): String {
+    val minutes: Int = seconds / 60
+    val updSeconds: Int = seconds % 60
+
+    return if (updSeconds < 10) "$minutes:0$updSeconds"
+    else "$minutes:$updSeconds"
+}
