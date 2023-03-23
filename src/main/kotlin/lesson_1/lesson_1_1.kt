2b514f3c584a612353f3a99f18ddfbdf39c9e579
Vibriya <ritaboyko906151@gmail.com>
2023-03-23 20:22:34 +0200

    KS-1-1 wrapped in the main function

--- src/main/kotlin/lesson_1/lesson_1_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_1.kt
@@ -1,4 +1,6 @@
 package lesson_1
 
-var countOfOrder: Int = 75
-const val gratefulMessage: String = "May Allah reward you greatly"
+fun main() {
+    var countOfOrder: Int = 75
+    val gratefulMessage: String = "May Allah reward you greatly"
+}
