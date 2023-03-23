95f2c19ef72609751d837a70e81654a2d97d28f3
Vibriya <ritaboyko906151@gmail.com>
2023-03-23 18:16:08 +0200

    KS-1-2 added variable output and personnel counter

--- src/main/kotlin/lesson_1/lesson_1_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_2.kt
@@ -1,2 +1,14 @@
 package lesson_1
 
+fun main() {
+    val countOfOrder: Int = 75
+    val gratefulMessage: String = "May Allah reward you greatly"
+
+    println("The number of orders: $countOfOrder")
+    println("Gratitude: $gratefulMessage")
+
+    var hiredStaff: Int = 2000
+//  println(hiredStaff)
+    --hiredStaff
+    println("The number of staff employed: $hiredStaff")
+}
