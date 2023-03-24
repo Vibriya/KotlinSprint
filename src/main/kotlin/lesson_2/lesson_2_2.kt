a0c784b9fed5303eae4bd16fb5e676456d2c89f4
Vibriya <ritaboyko906151@gmail.com>
2023-03-24 12:43:28 +0300

    KS-2-3 file cleaning

--- src/main/kotlin/lesson_2/lesson_2_2.kt
+++ src/main/kotlin/lesson_2/lesson_2_2.kt
@@ -1,13 +1,2 @@
 package lesson_2
 
-fun main() {
-    val departureHour = 9
-    val departureMinute = 39
-    val travelTime = 457
-
-    val allMinutes = departureMinute + travelTime
-    val arrivalHour = departureHour + allMinutes / 60
-    val arrivalMinute = allMinutes % 60
-
-    println("$arrivalHour:$arrivalMinute")
-}
