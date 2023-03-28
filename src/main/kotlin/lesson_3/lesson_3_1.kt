f08f49c1b0c91fe0138615172d7f6ccb11a504b5
Vibriya <ritaboyko906151@gmail.com>
2023-03-28 15:42:38 +0300

    KS-3-1 welcome message added

--- src/main/kotlin/lesson_3/lesson_3_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_1.kt
@@ -1,2 +1,9 @@
 package lesson_3
 
+fun main() {
+    val username = "killer3000"
+    var greetings = "Good day"
+    println("$greetings, $username!")
+    greetings = "Good night"
+    println("$greetings, $username!")
+}
